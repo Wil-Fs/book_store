7ea1cff11a39316d132338c04e626b88e712f7d9
William <williamfs133@gmail.com>
2024-06-11 16:07:01 -0300

    change: git-actions

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -144,7 +144,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-SECRET_KEY = os.environ.get("SECRET_KEY", '3333')
+SECRET_KEY = os.environ.get("SECRET_KEY", "3333")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
@@ -152,5 +152,5 @@ ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
 
 DEBUG_TOOLBAR_CONFIG = {
     # ... suas outras configurações da debug toolbar ...
-    'IS_RUNNING_TESTS': False,
+    "IS_RUNNING_TESTS": False,
 }
